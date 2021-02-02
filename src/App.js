86d21aa275a0d583e0332a23cf4061abe2db5938
Duyan-Kim <arbolkim@gmail.com>
2021-02-02 20:21:23 +0900

    [test] axios로 데이터 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { API_KEY } from "./API_KEY";
+
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios.get(API_KEY).then((response) => {
+      setData(response.data);
+    });
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
